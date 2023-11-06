d26aba131687eb6a13ca95c06963734b07b5be8c
Arnisfet <mrudge@student.21-school.ru>
2023-11-06 17:05:23 +0300

    Good things coming! Count of the whole and exclusive fils were made. Need to do the json part

--- test_pos_s/src/main/scala/Main.scala
+++ test_pos_s/src/main/scala/Main.scala
@@ -8,15 +8,34 @@ object Main {
   private val general_path = "u.data"
   private val exclusive_number = "328"
 
+/*** The counter function for the films' raitings ***/
+  def counter(source: Array[Int]) : Array[Int] = {
+    var result1: Array[Int] = Array()
+    for (i <- 1 to  5)
+      result1 = result1 :+ source.count(_ == i)
+    result1
+  }: Array[Int]
+
   def main(args: Array[String]): Unit = {
     var sequence = Array[String]()
     val source = Source.fromFile(local_path)
+    var result1: Array[Int] = Array()
+    var result2: Array[Int] = Array()
+
     /*Seems like it is not a good way to realloc array every time. Greedy solution*/
     for (line <- source.getLines())
         sequence = sequence :+ line
-    val init_source = sequence.map(string => string.split("\t").slice(1,3))
-//    val count_all_source = ini
     source.close()
+    /* Init array which requires for the whole calculations */
+    val init_source = sequence.map(string => string.split("\t").slice(1,3))
+    /* First part with all the films */
+    val all_rait_int = init_source.flatMap(string => string.slice(1, 2)).map(_.toInt)
+    result1 = counter(all_rait_int)
+    /* Second part with all the exclusive films */
+    val exclusive_rait = init_source.filter(_.apply(0) == exclusive_number)
+      .flatMap(string => string.slice(1, 2)).map(_.toInt)
+    result2 = counter(exclusive_rait)
+
   }
 
 
