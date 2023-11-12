988a78509a09d3996de697584a76680b344d2de6
Arnisfet <mrudge@student.21-school.ru>
2023-11-12 19:01:29 +0300

    Lab02-Step01: Finished parsing of usersfile, started domain parse process

--- lab02/src/main/scala/Main.scala
+++ lab02/src/main/scala/Main.scala
@@ -1,21 +1,40 @@
-import org.apache.spark.sql.functions.lit
-import org.apache.spark.sql.{Column, DataFrame, SparkSession}
+import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
+import org.apache.spark.sql.functions.{col, explode, from_json, lit, split}
+import org.apache.spark.sql.types.{ArrayType, StringType, StructField, StructType}
+import org.apache.spark.sql.{Column, DataFrame, Dataset, SparkSession}
 
 object Main {
+  private val local_path = "extra\\"
+  private val cluster_path = ""
+  private val autousers = "autousers.json"
+  private val logs = "logs"
+
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
       .master("local")
-      .appName("First app")
+      .appName("Lab02")
       .getOrCreate()
-    println("Hello world!")
 
-    val df = spark.range(start = 1, end = 10, step = 1, numPartitions = 1).toDF()
-    val retDf = addColumn("test", lit("test1"), df)
-    retDf.show(10)
-  }
+    val usersSchema =
+      StructType(
+        List(
+          StructField("autousers", ArrayType(StringType, containsNull = false))
+        )
+      )
+    val users = spark.read.schema(usersSchema).json(local_path + autousers)
+    /*** Explode way to split data in array[string] ***/
+    val dfSplit = users.select(explode(col("autousers")).as("autousers")).show()
+    /*** Split way to split data in array[string]  - does not work((, maybe split works only with String type***/
+    //    val dfSplit = users.select(split(col("autousers"), ",").as("autousers")).show()
 
-  def addColumn(name: String, c: Column, df: DataFrame): DataFrame ={
-    val returnDF = df.withColumn(name, c)
-    returnDF
-  }
+    val logsSchema =
+    StructType(
+      List(
+        StructField("uid", StringType),
+        StructField("ts", StringType),
+        StructField("url", StringType)
+      )
+    )
+    val domains = spark.read.schema(logsSchema).option("delimeter", "\t").csv(local_path + logs)
+    }
 }
